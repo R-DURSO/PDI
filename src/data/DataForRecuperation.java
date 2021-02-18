07849d075b6ae519a9b48feed053fc0dd91371c8
knbernard <60677447+knbernard@users.noreply.github.com>
2021-02-18 11:46:16 +0100

    Add MySQL SQL request

--- src/data/DataForRecuperation.java
+++ src/data/DataForRecuperation.java
@@ -1,5 +1,8 @@
 package data;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class DataForRecuperation {
 
 	// static variable for connection on mysql database
@@ -19,3 +22,9 @@ public class DataForRecuperation {
 	public static String CSV_ALL ="src/CSV/germany_1.csv";
 	public static String CSV_ALL2 ="src/CSV/germany_2.csv";
 }
+
+String ribccreq = " SELECT rib FROM CompteCourant WHERE idUtilisateur = ?" ;
+PreparedStatement ribreq =  conn.prepareStatement(ribccreq,ResultSet.TYPE_SCROLL_INSENSITIVE,
+	ResultSet.CONCUR_READ_ONLY,ResultSet.HOLD_CURSORS_OVER_COMMIT);
+ribreq.setInt(1, iduser);
+ResultSet rescc = ribreq.executeQuery();
--- src/data/SQLQuery.java
+++ src/data/SQLQuery.java
@@ -1,17 +1,20 @@
 package data;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class SQLQuery {
 	// string query of information's paie and static
 
 	// MySQL BD
-	public static String ORDER_DO_MYSQL = "";
-	public static String AVERAGE_WAGES_MYSQL = "";
-	public static String FRIDAY_USING_MYSQL = "";
-	public static String MONTH_EMPLOYE_MYSQL = "";
-	public static String OVERPAID_EMPLOYE_MYSQL = "";
+	public static String ORDER_DO_MYSQL = "SELECT achievements FROM Work";
+	public static String AVERAGE_WAGES_MYSQL = "SELECT salary, soc_benefits, blame FROM Pay NATURAL JOIN Work";
+	public static String FRIDAY_USING_MYSQL = "SELECT vacation FROM Work";
+	public static String MONTH_EMPLOYE_MYSQL = "SELECT employee_id, achievements, blame FROM Work";
+	public static String OVERPAID_EMPLOYE_MYSQL = "SELECT employee_id, salary, bonus, fees, tax FROM Pay";
 	public static String FORMATION_UTILITY_MYSQL = "";
-	public static String RESULT_BY_ANCIENTY_MYSQL = "";
-	public static String TYPE_OF_CONTRACT_MYSQL = "";
+	public static String RESULT_BY_ANCIENTY_MYSQL = "SELECT achievements, hiring_date FROM Work NATURAL JOIN Contract";
+	public static String TYPE_OF_CONTRACT_MYSQL = "SELECT contract FROM Contract";
 	public static String COST_OF_EMPLOY_MYSQL = "";
 
 	// PostGrey BD
@@ -24,5 +27,4 @@ public class SQLQuery {
 	public static String RESULT_BY_ANCIENTY_POSRTGRESQL = "";
 	public static String TYPE_OF_CONTRACT_POSRTGRESQL = "";
 	public static String COST_OF_EMPLOY_POSRTGRESQL = "";
-
 }

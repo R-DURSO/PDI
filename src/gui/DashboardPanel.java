aa21ba7f23df4be29ead912febf883c293d65948
rdurs <rdurso@outlook.fr>
2021-03-17 20:06:10 +0100

    WIP première Jpanel de dashboard

--- src/gui/DashboardPanel.java
+++ src/gui/DashboardPanel.java
@@ -8,6 +8,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.*;
@@ -30,24 +31,24 @@ public class DashboardPanel extends JPanel{
 			
 		}
 		
-		public JPanel creatTasksDonePanel() {
-			new JPanel();
+		public void creatTasksDonePanel() {
+	 	new JPanel();
 			setSize(800,600);
-			result=mediator.TasksDone();
-			System.out.println(result.getPedagogie());
+			//result=mediator.TasksDone();
+//			System.out.println(result.getPedagogie());
 			GridLayout resultLayout = new GridLayout(1,2);
 			setLayout(resultLayout);
-			add(creaeJTextArea(result.getPedagogie()));
+			add(creaeJTextArea("test"));
 			// creation du cammember 
 			DefaultPieDataset  pieDataset = new DefaultPieDataset();
 			pieDataset.setValue("GER", 0); // value of german succurale
 			pieDataset.setValue("USA", 0); // value of usa succurale
 			pieDataset.setValue("CHN", 0); // value of china succurale
 			JFreeChart pieChart = ChartFactory.createPieChart("taskdone per Succurale", pieDataset, true, false, false);
-			
+			ChartPanel cPanel = new ChartPanel(pieChart); 
+			add(cPanel);
 			repaint();
-			
-			
+		
 		}
 		
 		public JPanel creatWagesInfosPanel() {

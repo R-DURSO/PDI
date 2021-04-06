646fb11f85db64ff286555b2345c3ed80786b376
knbernard <60677447+knbernard@users.noreply.github.com>
2021-04-06 06:44:12 +0200

    Some comments added !

--- src/gui/DashboardPanel.java
+++ src/gui/DashboardPanel.java
@@ -27,6 +27,13 @@ import data.Pedagogy;
 import process.Mediator;
 import logger.LoggerUtility;
 
+/**
+ * This class process the elements display on the dashboard panel
+ * 
+ * @author Kevin BERNARD, Raphael D'URSO, Laura FUSTINONI, Aelien MOUBECHE
+ * @version
+ *
+ */
 public class DashboardPanel extends JPanel {
 
 //		private ElementManager manager;
@@ -36,69 +43,122 @@ public class DashboardPanel extends JPanel {
 	private MediatorResult result;
 	boolean isUser = false;
 
+	/**
+	 * Constructor.
+	 * This method allows to build the dashboard panel
+	 */
 	public DashboardPanel() {
+		
 		JPanel sartPanel = new JPanel();
 		sartPanel.setSize(800, 800);
+		
 	}
 
+	/**
+	 * This method creates the tasks done panel elements on the dashboard
+	 */
 	public void creatTasksDonePanel() {
+		
 		if (!isUser) {
 			result = mediator.TasksDone();
 			createCicurlarPanel(result);
 			isUser=true;
 		}
+		
 	}
 
+	/**
+	 * This method creates the wages information panel elements on the dashboard
+	 */
 	public void creatWagesInfosPanel() {
 
 	}
 
+	/**
+	 * This method creates the leave usage panel elements on the dashboard
+	 */
 	public void creatLeaveUsagePanel() {
+		
 		result = mediator.leaveUsage();
 		createTextPanel(result);
+		
 	}
 
+	/**
+	 * This method creates the month employee panel elements on the dashboard
+	 */
 	public void creatMonthEmpPanel() {
 
 	}
 
+	/**
+	 * This method creates the expensive employee panel elements on the dashboard
+	 */
 	public void creatExpensiveEmpPanel() {
 
 	}
 
+	/**
+	 * This method creates the formation utility panel elements on the dashboard
+	 */
 	public void creatFormationUtilityPanel() {
 
 	}
 
+	/**
+	 * This method creates the result by seniority panel elements on the dashboard
+	 */
 	public void creatResBySeniorityPanel() {
 
 	}
 
+	/**
+	 * This method creates the type of contract panel elements on the dashboard
+	 */
 	public void creatTypeContractPanel() {
 
 	}
 
+	/**
+	 * This method creates the employ cost panel elements on the dashboard
+	 */
 	public void creatEmployCostPanel() {
 
 	}
 
+	/**
+	 * This method creates the salary grades panel elements on the dashboard
+	 */
 	public void creatSalGradesPanel() {
+		
 		result = mediator.SalaryNote();
 		createListPanel(result);
+		
 	}
 
+	/**
+	 * This method creates the pay panel elements on the dashboard
+	 */
 	public void creatPayPanel() {
 
 	}
 
+	/**
+	 * This method allows to create a JTextArea on the dashboard
+	 * 
+	 * @param text : the text to put in the JTextArea in String
+	 * @return JTextArea containing the text
+	 */
 	public JTextArea creaeJTextArea(String text) {
-			JTextArea jTextArea = new JTextArea(text);
-			jTextArea.setEnabled(false);
-			jTextArea.setForeground(Color.WHITE);
-			jTextArea.setLineWrap(true);
-			jTextArea.setBackground(Color.BLACK);
-			return jTextArea;
-		}
+		
+		JTextArea jTextArea = new JTextArea(text);
+		jTextArea.setEnabled(false);
+		jTextArea.setForeground(Color.WHITE);
+		jTextArea.setLineWrap(true);
+		jTextArea.setBackground(Color.BLACK);
+		return jTextArea;
+		
+	}
 	/*
 	 * Prototypes histogramme
 	 * 
@@ -117,7 +177,14 @@ public class DashboardPanel extends JPanel {
 		
 
 	*/
+	
+	/**
+	 * This method allows to create a panel with a circular graphic on the dashboard
+	 * 
+	 * @param result : result of the mediator
+	 */
 	public void createCicurlarPanel(MediatorResult result) {
+		
 		setSize(800, 600);
 		GridLayout resultLayout = new GridLayout(1, 2);
 		setLayout(resultLayout);
@@ -137,9 +204,13 @@ public class DashboardPanel extends JPanel {
 		
 	}
 	
-
-	
-	public void createBarCharGraphic(MediatorResult result){
+	/**
+	 * This method allows to create a panel with a bar char graphic on the dashboard
+	 * 
+	 * @param result : result of the mediator
+	 */
+	public void createBarCharGraphic(MediatorResult result) {
+		
 		setSize(800, 600);
 		GridLayout resultLayout = new GridLayout(1, 2);
 		setLayout(resultLayout);
@@ -153,16 +224,31 @@ public class DashboardPanel extends JPanel {
 		 ChartPanel chartPanel = new ChartPanel( barChart ); 
 		 add(chartPanel);
 		 isUser=true;
+		 
 	}
+	
+	/**
+	 * This method allows to create a text panel with information on the dashboard
+	 * 
+	 * @param result : result of the mediator
+	 */
 	public void createTextPanel(MediatorResult result) {
+		
 		setSize(800, 600);
 		GridLayout resultLayout = new GridLayout(1, 2);
 		setLayout(resultLayout);
 		add(creaeJTextArea(result.getPedagogie()));
 		add(creaeJTextArea(result.getInformation()));
+		
 	}
 	
+	/**
+	 * This method allows to create a list panel on the dashboard
+	 * 
+	 * @param result : result of the mediator
+	 */
 	public void createListPanel(MediatorResult result) {
+		
 		setSize(800, 600);
 		GridLayout resultLayout = new GridLayout(1, 2);
 		setLayout(resultLayout);
@@ -174,4 +260,5 @@ public class DashboardPanel extends JPanel {
 		add( creaeJTextArea(liString));
 		
 	}
+	
 }
